310853e725ee49b09fa489e297895d0dc14ddf9b
unknown <rb357rainbow@gmail.com>
2020-08-11 09:25:56 +0300

    ikinci satir ekledim

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -1,5 +1,9 @@
 package git_project_ikinci;
 
 public class deneme {
-
+    public static void main(String[] args) {
+	System.out.println("ikinci git projesi");
+	System.out.println("ikinci satir kodum");
 }
+	}
+
