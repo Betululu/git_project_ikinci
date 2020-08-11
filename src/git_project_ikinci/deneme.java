6c9ae0defcf0ea2514e8467a8926510383927b7a
Betululu <69423877+Betululu@users.noreply.github.com>
2020-08-11 09:33:32 +0300

    Update deneme.java
    githubta degisiklik yaptim

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -4,6 +4,7 @@ public class deneme {
     public static void main(String[] args) {
 	System.out.println("ikinci git projesi");
 	System.out.println("ikinci satir kodum");
+	System.out.println("remote degisiklik");   
 }
 	}
 
